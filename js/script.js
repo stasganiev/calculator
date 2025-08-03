1e2bcc44da983211a6ab56ef08a7a08e5ef55713
Stas Ganiev <stas@ganiev.pro>
2025-08-03 08:48:54 +0200

    feat: start operations

--- js/script.js
+++ js/script.js
@@ -9,9 +9,9 @@ const display = {
     operand1: 0,
     operand2: 0,
     dotIsExist: false,
-    sign: false
+    sign: false,
+    currentOperation: ''
 };
-const operations = ['add', 'sub', 'mul', 'div'];
 const maxLength = 12;
 
 const allButtons = document.querySelectorAll('.key');
@@ -78,17 +78,18 @@ function reset() {
     display.operand2 = 0;
     display.dotIsExist = false;
     display.sign = false;
+    display.currentOperation = '';
 }
 
-function operate(val1, val2, operationIndex) {
-    switch (operationIndex) {
-        case 0:
+function operate(val1, val2, operation) {
+    switch (operation) {
+        case 'add':
             return val1 + val2;
-        case 1:
+        case 'sub':
             return val1 - val2;
-        case 2:
+        case 'mul':
             return val1 * val2;
-        case 3:
+        case 'div':
             return (val2 === 0) ? 0 : val1 / val2;
     }
 }
@@ -97,11 +98,12 @@ function initiolize() {
     for (let btnItem of allButtons) {
         btnItem.addEventListener('click', keyOnClick);
     }
+    reset();
 }
 
 function pressDigit(keyCode) {
     if (display.mode === 'waiting') {
-        display.content += keyCode;
+        display.content = keyCode;
         display.mode = 'numberinput';
     } else if (display.mode === 'numberinput') {
         if (display.content.length < maxLength) display.content += keyCode;
@@ -109,6 +111,14 @@ function pressDigit(keyCode) {
 }
 
 function pressOperation(keyCode) {
+    if (display.mode === 'waiting') {
+        display.operand1 = 0;
+        display.currentOperation = keyCode;
+    } else if (display.mode === 'numberinput') {
+        display.operand1 = +display.content * (display.sign) ? (-1) : 1;
+        display.currentOperation = keyCode;
+        display.mode = 'waiting';
+    }
 }
 
 function pressDone(keyCode) {
@@ -131,11 +141,15 @@ function pressOther(keyCode) {
             display.content += '.';
             display.dotIsExist = true;
         }
+    } else if (keyCode === 'sign') {
+        display.sign = !display.sign;
     }
 }
 
 function updateDisplay() {
-    displayItem.value = display.content;
+    let res = (display.sign) ? '-' : '';
+    res += display.content;
+    displayItem.value = res;
 }
 
 // Module code
